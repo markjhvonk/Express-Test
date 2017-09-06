41e9a9a2f0fc8e9aa6cde000710512ff845c3006
Mark Vonk <markjhvonk@gmail.com>
2017-09-06 17:32:01 +0200

    Database connection + node modules update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'laraveltest'
 });
 
-module.exports = router;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('index', { title: 'Testing tasks!' });
+});
+
+router.get('/tasks', function(req, res, next) {
+  
+  var tasks = [];
+    
+  connection.query('SELECT * FROM tasks', function (err, rows, fields) {
+    if (err) throw err;
+
+    for(var k=0;k<rows.length;k++){
+      tasks.push(rows[k].body)
+    }
+    // console.log(rows[0].body);
+  });
+  
+  console.log(tasks);
+  
+  res.render('tasks', { tasksData: tasks });
+  res.render('tasks', { title: 'Testing tasks!' });
+});
+
+module.exports = router
